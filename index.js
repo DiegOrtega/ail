8f71d0cf47f18f076b23947d8af735e8b31f0ace
DiegOrtega <nemesisrda@gmail.com>
2016-08-11 17:24:52 -0500

    Actualizacion plantillas

--- index.js
+++ index.js
@@ -13,8 +13,46 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get("/tienda-home", function(request,response){
+   response.render("pages/ecommerce"); 
+});
+
+app.get("/agencia", function(request, response){
+   response.render("pages/agency") 
+});
+
+app.get("/blog-simple", function(request, response){
+   response.render("pages/blog-simple") 
+});
+
+app.get("/blog", function(request, response){
+   response.render("pages/blog") 
+});
+
+app.get("/sitio-marketing", function(request, response){
+   response.render("pages/marketing-site") 
+});
+
+app.get("/noticias", function(request, response){
+   response.render("pages/news-magazine") 
+});
+
+app.get("/portafolio", function(request, response){
+   response.render("pages/portfolio") 
+});
+
+app.get("/pagina-producto", function(request, response){
+   response.render("pages/product-page") 
+});
+
+app.get("/real-estate", function(request, response){
+   response.render("pages/real-estate") 
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
 
 
